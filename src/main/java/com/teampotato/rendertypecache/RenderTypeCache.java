8fa7e816a512ac7efccf7eedac553b1366cf0923
Kasualix <670067575@qq.com>
2023-08-23 03:15:57 +0800

    Initial commit

---
+++ src/main/java/com/teampotato/rendertypecache/RenderTypeCache.java
@@ -0,0 +1,10 @@
+package com.teampotato.rendertypecache;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("rendertypecache")
+public class RenderTypeCache {
+    public static final Logger LOGGER = LogManager.getLogger();
+}
